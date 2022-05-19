0f019f4eebe46228f1db9e314cfba7047461de81
ankush <adeankush3@gmail.com>
2022-05-19 19:47:56 +0530

    [Ankush]Add user and Login User done

---
+++ FundooNotes/BussinessLayer/Interfaces/IUserBL.cs
@@ -0,0 +1,17 @@
+﻿using DataBaseLayer.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Interfaces
+{
+    public interface IUserBL
+    {
+        /// <summary>
+        /// seme method used here from IUserRL
+        /// </summary>
+        /// <param name="user"></param>
+        public void AddUser(UserModel user);
+        public string LoginUser(string email, string password);
+    }
+}
